44f5bd38c3d0bf5c79d9f106987583b4d72def6f
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-07-11 23:15:24 +0600

    navbar edited

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import { FaEnvelopeOpen } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
+    <div className="navbar bg-gray-100 border-b-2 border-gray-200">
+      <div className="navbar-start w-8/12">
         <div className="dropdown">
-          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+          <label tabindex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -16,14 +16,14 @@ const Navbar = () => {
             >
               <path
                 strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
+                stroke-linejoin="round"
+                stroke-width="2"
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
           </label>
           <ul
-            tabIndex="0"
+            tabindex="0"
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
@@ -37,7 +37,7 @@ const Navbar = () => {
                 Sell
               </Link>
             </li>
-            <li tabIndex="0">
+            <li tabindex="0">
               <Link to="" className="justify-between font-bold">
                 Manage Property
                 <svg
@@ -63,7 +63,7 @@ const Navbar = () => {
                 </li>
               </ul>
             </li>
-            <li tabIndex="0">
+            <li tabindex="0">
               <Link to="" className="justify-between">
                 Resources
                 <svg
@@ -90,73 +90,73 @@ const Navbar = () => {
         <Link to="" className="btn btn-ghost normal-case text-xl font-bold">
           <FaEnvelopeOpen className="text-primary mr-2" /> Estatery
         </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link to="" className="font-bold">
-              Rent
-            </Link>
-            <Link to="" className="font-bold">
-              Buy
-            </Link>
-            <Link to="" className="font-bold">
-              Sell
-            </Link>
-          </li>
-          <li tabIndex="0">
-            <Link to="" className="font-bold">
-              Manage Property
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </Link>
-            <ul className="p-2">
-              <li>
-                <Link to="" className="font-bold">
-                  Submenu 1
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="font-bold">
-                  Submenu 2
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li tabIndex="0">
-            <Link to="" className="font-bold">
-              Resources
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </Link>
-            <ul className="p-2">
-              <li>
-                <Link to="" className="font-bold">
-                  Submenu 1
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="font-bold">
-                  Submenu 2
-                </Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <div className=" hidden lg:flex mx-auto">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link to="" className="font-bold">
+                Rent
+              </Link>
+              <Link to="" className="font-bold">
+                Buy
+              </Link>
+              <Link to="" className="font-bold">
+                Sell
+              </Link>
+            </li>
+            <li tabindex="0">
+              <Link to="" className="font-bold">
+                Manage Property
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </Link>
+              <ul className="p-2 bg-gray-100">
+                <li>
+                  <Link to="" className="font-bold">
+                    Submenu 1
+                  </Link>
+                </li>
+                <li>
+                  <Link to="" className="font-bold">
+                    Submenu 2
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li tabindex="0">
+              <Link to="" className="font-bold">
+                Resources
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </Link>
+              <ul className="p-2">
+                <li>
+                  <Link to="" className="font-bold">
+                    Submenu 1
+                  </Link>
+                </li>
+                <li>
+                  <Link to="" className="font-bold">
+                    Submenu 2
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="navbar-end mr-3">
         <Link to="" className="btn btn-sm btn-outline btn-primary mr-3">
