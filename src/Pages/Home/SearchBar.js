405cd6b26399e2eeda69fc4feb5f8d711dd14a89
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-07-11 22:37:00 +0600

    search bar edited

--- src/Pages/Home/SearchBar.js
+++ src/Pages/Home/SearchBar.js
@@ -2,11 +2,11 @@ import React from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 const SearchBar = () => {
   return (
-    <div className="flex justify-around mt-10">
+    <div className="flex flex-col md:flex-row lg:flex-row justify-around items-center mt-10">
       <p className="text-4xl font-bold">Search properties to rent</p>
-      <div className="flex bg-white border-2 border-gray-300 rounded-md px-3">
+      <div className="flex justify-between items-center w-64 h-11 bg-white border-2 border-gray-300 rounded-md mt-3 md:mt-0 lg:mt-0 px-3">
         <input
-          className="font-bold placeholder:font-bold w-full max-w-xs focus:outline-none"
+          className="font-bold placeholder:font-bold focus:outline-none"
           type="text"
           placeholder="Search with Search Bar"
         />
