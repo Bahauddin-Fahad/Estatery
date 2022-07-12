85040c818cc2f58a1039f702dade052004b8da6d
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-07-12 14:09:30 +0600

    search button implemented

--- src/Pages/Home/SearchFilters.js
+++ src/Pages/Home/SearchFilters.js
@@ -28,9 +28,9 @@ const SearchFilters = () => {
           <p className="text-start text-gray-400 text-sm font-bold">Price</p>
           <select className="select select-ghost w-full max-w-xs pl-0 font-bold focus:outline-none">
             <option value="$500-$2,000">$500-$2,000</option>
-            <option>$2,000-$3,100</option>
-            <option>$3,100-$4,400</option>
-            <option>$4,400-$6,000</option>
+            <option>$2,000-$3,200</option>
+            <option>$3,200-$4,500</option>
+            <option>$4,500-$6,000</option>
           </select>
         </div>
         <div className="h-0 md:h-12 lg:h-12 w-0 md:w-[2px] lg:w-[2px] rounded-full bg-gray-300 my-auto"></div>
